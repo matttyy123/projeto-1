d1d19bec88636996bbed71111439470f7d060d09
matttyy123 <132579496+matttyy123@users.noreply.github.com>
2024-05-15 09:54:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-15T23:59:59");
-const tempoObjetivo2 = new Date("2024-05-17T23:59:59");
-const tempoObjetivo3 = new Date("2024-10-30T23:59:59");
+const tempoObjetivo1 = new Date("2025-01-01T23:59:59");
+const tempoObjetivo2 = new Date("2025-01-01T23:59:59");
+const tempoObjetivo3 = new Date("2025-01-01T23:59:59");
 const tempoObjetivo4 = new Date("2025-01-01T23:59:59");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
